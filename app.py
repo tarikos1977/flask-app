b28a406ed2f85ac86ef9f3615b70aa901aa44936
ouazar <t.ouazar@gmail.com>
2024-03-23 04:10:20 +0000

    first modification

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-if __name__ == '__main__':
+@app.route('/<name>')
+def greet(name):
+    return f'Hello, {name}!'    
+
+@app.route('/bye')
+def goodbye():  
+    return 'Goodbye!' 
+
+if __name__ == "__main__":
     app.run(debug=True)
+
